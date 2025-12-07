5685b914c85c4aa2166f0f601ff6f01839d9d403
Sujit-Anireddy <anireddysujitreddy@gmail.com>
2025-12-07 15:20:17 -0600

    updated time complexity

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,10 +1,13 @@
 #Writing Bubble Sort from Scratch 
 #Refrence - https://en.wikipedia.org/wiki/Bubble_sort
+#Time Complexity: O(n**2)
 
-#Pseudocode
-#   1. Loop through the list and compare adjacent numbers, if the number to the right is less than the number to the left -> Swap them.
-#       Example: [6,3,2,1] -> Here for the first comparison the number to the right is 3 and number to the left is 6 and 3 < 6, so we swap -> [3,6,2,1].
-#   2. Step 1 is repeated until all elements are sorted.
+"""
+Pseudocode
+1. Loop through the list and compare adjacent numbers, if the number to the right is less than the number to the left -> Swap them.
+    Example: [6,3,2,1] -> Here for the first comparison the number to the right is 3 and number to the left is 6 and 3 < 6, so we swap -> [3,6,2,1].
+2. Step 1 is repeated until all elements are sorted.
+"""
 
 def bubble_sort(nums):
 
