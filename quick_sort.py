81c71ddfa52e00269efc3677ffd506cc404fe71c
Sujit-Anireddy <anireddysujitreddy@gmail.com>
2025-12-11 17:07:41 -0600

    quick sort completed

---
+++ quick_sort.py
@@ -0,0 +1,36 @@
+#Writing Quick sort from scratch 
+#Date: Dec 11th 2025
+#Refrence: https://en.wikipedia.org/wiki/Quicksort
+
+
+#Nums is the list of numbers, low = 0, high = len(nums) - 1 (last index)
+def quick_sort(nums, low, high):
+
+    if low < high:
+
+        #Partition funtion will return the pivot index after placing the pivot index number in it's correct position
+        pivot_index = partition(nums, low, high)
+        
+        #recursivley calling partition on left side of the array 
+        quick_sort(nums, low, pivot_index -1 )
+
+        #recursivley calling partition on the right side of the array
+        quick_sort(nums, pivot_index + 1, high)
+
+
+def partition(nums, low, high):
+
+    #ith indx starts at -1
+    i = low - 1
+    pivot = nums[high]
+
+    for j in range(low, high):
+        if nums[j] < pivot:
+            i += 1 
+
+            #Swap
+            nums[i], nums[j] = nums[j], nums[i]
+    
+    #Placing the pivot element in it's sorted position
+    nums[i+1], nums[high] = nums[high], nums[i+1]
+    return i + 1
